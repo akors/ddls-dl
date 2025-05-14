86aa741f4b4419e958c973b7d023b76881fb96be
Alexander Korsunsky <alexander.korsunsky@liu.se>
2025-05-14 10:14:44 +0200

    model: Baseline architecture from Demo3

--- nn_model.py
+++ nn_model.py
@@ -4,16 +4,21 @@ from tensorflow.keras import datasets, layers, models
 def create_model():
     # %% convolutional layers
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.Conv2D(8, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.Conv2D(16, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.summary()
 
 
     # %% dense final layers
     model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(32, activation='relu'))
     model.add(layers.Dense(10))
+    model.summary()
 
     return model
