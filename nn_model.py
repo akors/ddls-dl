0049b82a7ebadf330acc21c7559636b5c99c192b
Alexander Korsunsky <alexander.korsunsky@liu.se>
2025-05-14 22:27:33 +0200

    model: naively add batchnorm after convolutions

--- nn_model.py
+++ nn_model.py
@@ -8,14 +8,18 @@ def create_model():
 
     # %% convolutional layers
     x = layers.Conv2D(8, (3, 3), input_shape=(32, 32, 3))(input)
+    x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
     x = layers.Conv2D(16, (3, 3))(x)
+    x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
     x = layers.MaxPooling2D((2, 2))(x)
     x = layers.Conv2D(32, (3, 3))(x)
+    x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
     x = layers.MaxPooling2D((2, 2))(x)
     x = layers.Conv2D(64, (3, 3))(x)
+    x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
     x = layers.MaxPooling2D((2, 2))(x)
 
