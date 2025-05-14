ffaf47015a1af8595849885ce62d3ac61cc84879
Alexander Korsunsky <alexander.korsunsky@liu.se>
2025-05-14 15:57:57 +0200

    model: extract activation as layers

--- nn_model.py
+++ nn_model.py
@@ -4,12 +4,16 @@ from tensorflow.keras import datasets, layers, models
 def create_model():
     # %% convolutional layers
     model = models.Sequential()
-    model.add(layers.Conv2D(8, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-    model.add(layers.Conv2D(16, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(8, (3, 3), input_shape=(32, 32, 3)))
+    model.add(layers.ReLU())
+    model.add(layers.Conv2D(16, (3, 3)))
+    model.add(layers.ReLU())
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(32, (3, 3)))
+    model.add(layers.ReLU())
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3)))
+    model.add(layers.ReLU())
     model.add(layers.MaxPooling2D((2, 2)))
     model.summary()
 
