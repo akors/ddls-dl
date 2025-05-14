17c6018886245fad2a7c969c1ebe89606916840c
Alexander Korsunsky <alexander.korsunsky@liu.se>
2025-05-14 16:12:42 +0200

    model: convert from sequential to functional style

--- nn_model.py
+++ nn_model.py
@@ -1,28 +1,31 @@
 
+from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
 
 def create_model():
-    # %% convolutional layers
-    model = models.Sequential()
-    model.add(layers.Conv2D(8, (3, 3), input_shape=(32, 32, 3)))
-    model.add(layers.ReLU())
-    model.add(layers.Conv2D(16, (3, 3)))
-    model.add(layers.ReLU())
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(32, (3, 3)))
-    model.add(layers.ReLU())
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3)))
-    model.add(layers.ReLU())
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.summary()
+    input = keras.Input(shape=(32, 32, 3), name="img")
 
+
+    # %% convolutional layers
+    x = layers.Conv2D(8, (3, 3), input_shape=(32, 32, 3))(input)
+    x = layers.ReLU()(x)
+    x = layers.Conv2D(16, (3, 3))(x)
+    x = layers.ReLU()(x)
+    x = layers.MaxPooling2D((2, 2))(x)
+    x = layers.Conv2D(32, (3, 3))(x)
+    x = layers.ReLU()(x)
+    x = layers.MaxPooling2D((2, 2))(x)
+    x = layers.Conv2D(64, (3, 3))(x)
+    x = layers.ReLU()(x)
+    x = layers.MaxPooling2D((2, 2))(x)
 
     # %% dense final layers
-    model.add(layers.Flatten())
-    model.add(layers.Dense(128, activation='relu'))
-    model.add(layers.Dense(32, activation='relu'))
-    model.add(layers.Dense(10))
-    model.summary()
+    x = layers.Flatten()(x)
+    x = layers.Dense(128, activation='relu')(x)
+    x = layers.Dense(32, activation='relu')(x)
+    output = layers.Dense(10)(x)
+
+
+    model = keras.Model(input, output, name="cnn")
 
     return model
